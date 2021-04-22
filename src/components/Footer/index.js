e861aaa12f3c78e5ef9a3ec0ebb35f569bac20ce
Jappreet Singh <32400976+jappreetS@users.noreply.github.com>
2021-04-22 17:51:43 +0530

    SideMenu, SignOut implemenation (#6)
    * added firebase app configs in .env
    
    * Firebase Context and Class setup
    
    * Google SignIn with Firebase basic implementation
    
    * saved loggedIn user data in Firebase DB
    
    * actions, reducers and store setup
    
    * onAuthChangeListener implementation with withAuthentication HOC and App
    
    * save loggedIn user data in redux from HOC and Redux Dev Tools implementation
    
    * routing and private route with withAuthorization HOC implementation
    
    * Header UI added
    
    * Footer UI added, Header UI updated
    
    * Side Menu UI
    
    * SignOut implementation
    
    * merged develop

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,52 @@
 import React from 'react';
 
+import { GITHUB_PROFILE, LINKEDIN_PROFILE } from './../../constants';
+
+import Icon from './../../elements/Icon';
+
+import './Footer.scss';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const handleIconClick = (targetName) => {
+    switch (targetName) {
+      case 'github':
+        window.open(GITHUB_PROFILE, '_blank');
+        return;
+      case 'linkedin':
+        window.open(LINKEDIN_PROFILE, '_blank');
+        return;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <div className="Footer d-flex justify-content-between">
+      <div>
+        Made with <span className="Footer-heart">&hearts;</span> by{' '}
+        <a href={GITHUB_PROFILE} target="_blank" rel="noreferrer">
+          Jappreet
+        </a>
+      </div>
+      <div>
+        <Icon
+          className="mr-16"
+          isDefaultName
+          name="github"
+          width="24"
+          height="24"
+          onClick={() => handleIconClick('github')}
+        />
+        <Icon
+          isDefaultName
+          name="linkedin"
+          width="24"
+          height="24"
+          onClick={() => handleIconClick('linkedin')}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import { HOME, SIGN_IN } from './../../constants/routes';
 
+import SideMenu from './../SideMenu';
+
 import CustomLink from './../../elements/CustomLink';
 import Icon from './../../elements/Icon';
 
@@ -13,11 +15,14 @@ const Header = (props) => {
   const { authUser, isHome } = props;
   const [firstName, setFirstName] = useState('');
   const [isHeadingClick, setIsHeadingClick] = useState(false);
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
 
   useEffect(() => {
     if (!!authUser && !!authUser.displayName) {
       const { displayName } = authUser;
       setFirstName(displayName.split(' ')[0]);
+    } else {
+      setFirstName('');
     }
   }, [authUser]);
 
@@ -27,20 +32,33 @@ const Header = (props) => {
         !!isHome ? 'white' : 'black'
       }`}
     >
-      <div className="d-flex align-items-center">
-        <Icon className="mr-16" name="hamburger" />
-        <h1
-          className="title cursor-pointer"
+      <div className="Header-left d-flex align-items-center">
+        <Icon
+          className="mr-16"
+          name="hamburger"
+          width={32}
+          height={32}
+          onClick={() => setIsSideMenuOpen(true)}
+        />
+        <div
+          role="button"
+          className="Header-left__title cursor-pointer"
           onClick={() => setIsHeadingClick(true)}
         >
           Little Tags
-        </h1>
+        </div>
       </div>
       {/* Search Bar */}
       <div className="Header-right d-flex align-items-center">
         {!!firstName ? (
           <>
-            <Icon className="mr-16" isClickable={false} name="user" />
+            <Icon
+              className="mr-16"
+              isClickable={false}
+              name="user"
+              width={32}
+              height={32}
+            />
             <div className="Header-right__name">{firstName}</div>
           </>
         ) : (
@@ -50,9 +68,14 @@ const Header = (props) => {
             path={SIGN_IN}
           />
         )}
-        <Icon className="ml-16" name="cart" />
+        <Icon className="ml-16" name="cart" width={32} height={32} />
       </div>
       {!!isHeadingClick && <Redirect to={HOME} />}
+      <SideMenu
+        userFirstName={!!firstName ? firstName : ''}
+        isSideMenuOpen={isSideMenuOpen}
+        handleCloseSideMenu={setIsSideMenuOpen}
+      />
     </div>
   );
 };
