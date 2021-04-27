f585ecae787960eb5b8627f941517e6084f1188f
Jappreet Singh <jappreet.singh@outlook.com>
2021-04-27 17:30:29 +0530

    Home page UI and SignIn styles updated

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
 import Hero from './../../components/Hero';
+import ProductCard from './../../components/ProductCard';
+
+import './Home.scss';
 
 const Home = () => {
   return (
     <div className="Home">
       <Hero />
+      <section className="Home-mostInDemand">
+        <div className="Home-mostInDemand__title text-align-center">
+          Most in Demand
+        </div>
+        <div className="Home-mostInDemand__products d-flex">
+          <div className="Home-mostInDemand__products-left">
+            <ProductCard caption="T-shirt" name="product-1" variant="primary" />
+            <ProductCard caption="Jeans" name="product-2" variant="primary" />
+          </div>
+          <div className="Home-mostInDemand__products-right d-flex">
+            <ProductCard
+              caption="Bagpack"
+              name="product-3"
+              variant="secondary"
+            />
+            <ProductCard
+              caption="Charm necklace"
+              name="product-4"
+              variant="secondary"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
